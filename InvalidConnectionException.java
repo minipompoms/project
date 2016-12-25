919e845293f957190ee7168d00436c11e043b5ce
minipompoms <pkohn@student.touro.edu>
2016-12-25 12:22:50 -0500

    Delete InvalidConnectionException.java

--- InvalidConnectionException.java
+++
@@ -1,9 +0,0 @@
-package project;
-
-public class InvalidConnectionException extends RuntimeException {
-
-	public InvalidConnectionException()
-	{
-		super("Exception Occured: unable to connect to file.");
-	}
-}
